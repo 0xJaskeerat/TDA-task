92716436b7ec081082935add9376faa73163934c
Jaskeerat Singh <jaskeerat10000@gmail.com>
2023-08-14 11:38:58 +0530

    fix: added relevant comments in code

--- app/src/components/ScenarioCharts.jsx
+++ app/src/components/ScenarioCharts.jsx
@@ -11,34 +11,58 @@ import {
 } from 'recharts';
 
 const ScenarioCharts = ({ response }) => {
-  const scenerios = ['scenerioA', 'scenerioB', 'scenerioC'];
-  const scenerioProperties = Object.keys(response.scenerioA.chartData)
+  // Define an array of scenarios
+  const scenarios = ['scenerioA', 'scenerioB', 'scenerioC'];
 
-  const scenerioData = scenerioProperties.map(property => {
+  // Getting the for the first scenarios in the response
+  const scenarioProperties = Object.keys(response.scenerioA.chartData);
+
+  // Create an array of data objects for each property and scenario combination
+  const scenarioData = scenarioProperties.map(property => {
     const data = { name: property };
-    scenerios.forEach(scenerio => {
-      data[scenerio] = response[scenerio]?.chartData?.[property] || 0;
+
+    // Populate data for each scenario
+    scenarios.forEach(scenario => {
+      data[scenario] = response[scenario]?.chartData?.[property] || 0;
     });
+
     return data;
   });
 
   return (
     <>
       <h2 style={{ textAlign: 'center' }}>Output Charts</h2>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
+      <div style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+      }}>
         <ResponsiveContainer width="80%" aspect={3}>
-          <BarChart width={500} height={300} data={scenerioData}>
+          {/* Create a bar chart with provided width, height, and data */}
+          <BarChart width={500} height={300} data={scenarioData}>
+            {/* Add a grid with dashed stroke */}
             <CartesianGrid strokeDasharray="3 3" />
+
+            {/* Set X-axis to display property names */}
             <XAxis dataKey="name" />
+
+            {/* Set Y-axis */}
             <YAxis />
+
+            {/* Display tooltips on hover */}
             <Tooltip />
+
+            {/* Display legend */}
             <Legend />
-            {scenerios.map((scenerio, index) => (
+
+            {/* Create Bar components for each scenario */}
+            {scenarios.map((scenario, index) => (
               <Bar
-                key={scenerio}
-                dataKey={scenerio}
-                fill={getColor(index)}
-                name={scenerio}
+                key={scenario}
+                dataKey={scenario}
+                fill={getColor(index)} // Apply a color to each bar based on index
+                name={scenario}
               />
             ))}
           </BarChart>
@@ -48,10 +72,10 @@ const ScenarioCharts = ({ response }) => {
   );
 };
 
-// Helper function to generate a random color
+// Helper function to generate a color based on index
 const getColor = (index) => {
   const colors = ['#EA1179', '#1A5D1A', '#FFB84C'];
-  return colors[index % colors.length];
+  return colors[index % colors.length]; // Cycle through colors based on index
 };
 
 export default ScenarioCharts;
--- app/src/components/UserInputForm.jsx
+++ app/src/components/UserInputForm.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import { Form, Input, Button, Row, Col } from 'antd';
-import { initialValues, additionalValues, formItems } from '../constants';
+import { initialValues, additionalValues, formItems } from '../common/constants.js';
 
+// Fetch API key and URL from environment variables using Vite's import.meta.env
 const API_KEY = import.meta.env.VITE_API_KEY;
 const API_URL = import.meta.env.VITE_API_URL;
 
 const UserInputForm = ({ setApiResponse }) => {
+    // Function to handle form submission
     const onFinish = async (values) => {
         try {
+            // Make a POST request to the API
             const response = await axios.post(
                 API_URL,
                 { ...values, ...additionalValues },
@@ -19,23 +22,26 @@ const UserInputForm = ({ setApiResponse }) => {
                     },
                 }
             );
+            // Set the API response data in the parent component's state
             setApiResponse(response?.data?.data);
         } catch (error) {
+            // Log any API errors
             console.error('API Error:', error);
         }
     };
 
+    // Function to handle form submission failure
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
 
     return (
         <>
-            {/* form */}
+            {/* Form component */}
             <Form
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                initialValues={initialValues}
+                onFinish={onFinish} // Handle form submission
+                onFinishFailed={onFinishFailed} // Handle form submission failure
+                initialValues={initialValues} // Set initial form values
                 style={{
                     display: 'flex',
                     flexDirection: 'column',
@@ -47,6 +53,7 @@ const UserInputForm = ({ setApiResponse }) => {
                     span: 8,
                 }}
             >
+                {/* Create form fields using Ant Design's Form.Item */}
                 <Row gutter={[16, 16]}>
                     {
                         formItems.map(({ label, name }) => (
@@ -56,12 +63,14 @@ const UserInputForm = ({ setApiResponse }) => {
                                     name={name}
                                     rules={[{ required: true, message: `Please input ${label}!` }]}
                                 >
+                                    {/* Input field with name and type */}
                                     <Input name={name} type="number" />
                                 </Form.Item>
                             </Col>
                         ))
                     }
                 </Row>
+                {/* Submit button */}
                 <Button 
                     type="primary" 
                     htmlType="submit" 
@@ -80,4 +89,4 @@ const UserInputForm = ({ setApiResponse }) => {
     );
 };
 
-export default UserInputForm;
+export default UserInputForm; // Export the UserInputForm component
